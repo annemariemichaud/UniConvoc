2008e8546760badaf8988bfa971f70ae1f6b8683
AMM <annemariemichaud@gmail.com>
2019-08-23 18:42:33 +0200

    enregistrement des fichiers pdf nommés avec les noms et prénoms des personnes convoquées

--- UniConvoc.py
+++ UniConvoc.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import PyPDF2
+import re
 
 pdfOMFile = open('om.pdf', 'rb')
 pdfOMReader = PyPDF2.PdfFileReader(pdfOMFile)
-
 
 for pageNum in range(pdfOMReader.numPages):
     pdfWriter = PyPDF2.PdfFileWriter()
     pageObj = pdfOMReader.getPage(pageNum)
     pdfWriter.addPage(pageObj)
-    name_new_file = 'OM' + '_' + str(pageNum+1) + '.pdf'
+    contenu_page=pageObj.extractText()
+    regexp='(Mme|M.)\\n-\\n[A-Z]*\\n-\\n[A-Z]*'
+    result = re.search(regexp, contenu_page)
+    if result:
+        name_new_file = 'OM' + '_' + result.group(0).replace('\n-\n','_')+ '.pdf'
+    else:
+        name_new_file = 'OM' + '_' + str(pageNum+1) + '.pdf'
     pdfOutputFile = open(name_new_file, 'wb')
     pdfWriter.write(pdfOutputFile)
     pdfOutputFile.close()
