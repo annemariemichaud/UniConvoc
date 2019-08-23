bb9d1dc39647cb17c67f6a49f74ce285bcabaeaa
AMM <annemariemichaud@gmail.com>
2019-08-23 18:59:22 +0200

    uniformisation des noms de fichiers sans . et sans espace

--- UniConvoc.py
+++ UniConvoc.py
@@ -10,12 +10,10 @@ for pageNum in range(pdfOMReader.numPages):
     pageObj = pdfOMReader.getPage(pageNum)
     pdfWriter.addPage(pageObj)
     contenu_page=pageObj.extractText()
-    regexp='(Mme|M.)\\n-\\n[A-Z]*\\n-\\n[A-Z]*'
+    regexp='(Mme|M.)\\n-\\n[A-Z\-]*\\n-\\n[A-Z\-]*'
     result = re.search(regexp, contenu_page)
     if result:
-        name_new_file = 'OM' + '_' + result.group(0).replace('\n-\n','_')+ '.pdf'
-    else:
-        name_new_file = 'OM' + '_' + str(pageNum+1) + '.pdf'
+        name_new_file = 'OM' + '_' + result.group(0).replace('\n-\n','_').replace('.','')+ '.pdf'
     pdfOutputFile = open(name_new_file, 'wb')
     pdfWriter.write(pdfOutputFile)
     pdfOutputFile.close()
