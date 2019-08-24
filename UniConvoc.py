f660375012353c24b07d1371d6597bc5749d2eb6
AMM <annemariemichaud@gmail.com>
2019-08-24 14:23:12 +0200

    enregistrement des pdf dans un dossier nommé sous la forme dispositif_module_groupe

--- UniConvoc.py
+++ UniConvoc.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
 import PyPDF2
 import re
-
+import os
 #ouverture du fichier pdf contenant toutes les convocations concaténées à la suite les unes des autres
 #le nom par défaut est om.pdf (généré automatiquemnt par GAIA)
 pdfOMFile = open('om.pdf', 'rb')
 pdfOMReader = PyPDF2.PdfFileReader(pdfOMFile)
+
+#extraction du numéro de dispositif, module et groupe
+pageObj = pdfOMReader.getPage(0)
+contenu_premiere_page=pageObj.extractText()
+regExpDispositif = 'Dispositif:[A-Z0-9]{10}'
+regExpModule = 'Module:[0-9]{5}'
+regExpGroupe = 'Groupedesession:[0-9]{2}'
+result_dispositif = re.search(regExpDispositif, contenu_premiere_page)
+if result_dispositif:
+    dispositif = result_dispositif.group(0).replace('Dispositif:','')
+else:
+    dipositif = 'inconnu'
+result_module = re.search(regExpModule, contenu_premiere_page)
+if result_module:
+    module = result_module.group(0).replace('Module:','')
+else:
+    module = 'inconnu'
+result_groupe = re.search(regExpGroupe, contenu_premiere_page)
+if result_groupe:
+    groupe=result_groupe.group(0).replace('Groupedesession:','')
+else:
+    groupe='inconnu'
+
+nom_repertoire = dispositif + '_' + module + '_' + groupe
+
+try:
+    os.mkdir(nom_repertoire)
+except FileExistsError:
+    print("Le répertoire  " , nom_repertoire ,  " existe déjà...") 
 
 #création des fichiers PDF à une seule page
 for pageNum in range(pdfOMReader.numPages):
@@ -20,9 +49,12 @@ for pageNum in range(pdfOMReader.numPages):
     if result:
     #le nom du fichier est OMNumeroPage_Mme_NOM_PRENOM ou OM_M_Nom_PRENOM
         name_new_file = 'OM' + str(pageNum+1) + '_' + result.group(0).replace('\n-\n','_').replace('.','')+ '.pdf'
-    pdfOutputFile = open(name_new_file, 'wb')
+    pdfOutputFile = open(nom_repertoire + '/' + name_new_file, 'wb')
     pdfWriter.write(pdfOutputFile)
     pdfOutputFile.close()
 
 pdfOMFile.close()
 
+
+
+   
